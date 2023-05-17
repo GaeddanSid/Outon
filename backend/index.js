7cbb9f22a1289293fc2a9b80038446a5f883c6b4
Pontus Norén Stomberg <pontus_noren91@hotmail.com>
2023-05-17 10:36:05 +0200

    Connected backend server

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,47 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const pg_1 = require("pg");
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const port = 8080;
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+const pool = new pg_1.Pool({
+    user: "kakswppo",
+    host: "balarama.db.elephantsql.com",
+    database: "kakswppo",
+    password: "s19uzg6SAcAwsM7G6Evb3WcAmTbmV7uJ",
+    port: 5432,
+});
+app.get("/products", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const result = yield pool.query("SELECT * FROM products");
+        res.json(result.rows);
+    }
+    catch (error) {
+        console.error("Cannot find data from database", error);
+        res.status(404).json({ error: "Something went wrong..." });
+    }
+}));
 app.get("/", (req, res) => {
     res.status(200).send("Testar testar!");
 });
--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,37 @@
 import express from "express";
+import { Pool } from "pg";
+import cors from "cors";
 
 const app = express();
 const port = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+const pool = new Pool({
+  user: "kakswppo",
+  host: "balarama.db.elephantsql.com",
+  database: "kakswppo",
+  password: "s19uzg6SAcAwsM7G6Evb3WcAmTbmV7uJ",
+  port: 5432,
+});
+
+app.get("/products", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM products");
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Cannot find data from database", error);
+    res.status(404).json({ error: "Something went wrong..." });
+  }
+});
 
 app.get("/", (req, res) => {
   res.status(200).send("Testar testar!");
